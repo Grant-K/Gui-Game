61bdd3bfb0191dd0fbf2fc73c420723c193f4ad4
GrantK <grantklees108@gmail.com>
2018-03-06 12:39:10 -0800

    Inital commit

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Game
+{
+    JFrame gameWindow = new JFrame("Snake");
+    final int X = 1000;
+    final int Y = 680
+    
+    public GUI()
+    {
+        gameWindow.setSize(X, Y);
+        window.setLocation(300, 180);
+        
+    }
+}
---
+++ Snake.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake
+{
+    public static void main(String[] args)
+    {
+        new Game();
+    }   
+}
