ab49b8e605f14811d96bb10d52888381332284e0
GrantK <grantklees108@gmail.com>
2018-03-07 11:35:50 -0800

    Atempting to implement a key listener.

--- Game.java
+++ Game.java
@@ -3,16 +3,30 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Game
+public class Game implements KeyListener
 {
     JFrame gameWindow = new JFrame("Snake");
     final int X = 1000;
-    final int Y = 680
-    
-    public GUI()
+    final int Y = 680;
+
+    public Game()
     {
         gameWindow.setSize(X, Y);
-        window.setLocation(300, 180);
-        
+        gameWindow.setLocation(300, 180);
+        gameWindow.addKeyListener(this);
+        gameWindow.setFocusable(true);
+        gameWindow.setFocusTraversalKeysEnabled(false);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        System.out.println("keyPressed");
+    }
+
+    public void keyReleased(KeyEvent e) {
+        System.out.println("keyReleased");
+    }
+
+    public void keyTyped(KeyEvent e) {
+        System.out.println("keyTyped");
     }
 }
