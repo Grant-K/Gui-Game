227f4cab6002daf7cfbb4d444dc1c1f97d3724f1
GrantK <grantklees108@gmail.com>
2018-03-14 13:55:53 -0700

    Working on intersections and adding 2 Jcomponents

--- Intersects.java
+++ Intersects.java
@@ -5,17 +5,20 @@ import javax.swing.*;
 
 public class Intersects
 {
+    private Graphics2D g2D;
     JFrame gameWindow = Snake.gameWindow;
     Game rect1 = Snake.rect1;
-    Game rect2 = Snake.rect2;
+    //Game rect2 = Snake.rect2;
+    //Rectangle hitBox = new Rectangle(rect2.getSize());
+    Shape rectangle = new Rectangle(rect1.getSize());
     //Rectangle rect2r = rect2.getClipBounds();
-    public Intersects()
-    {
-        Timer t = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if(rect2g.intersects(rect2g))
-                    System.out.println("They are intersecting!");
-            }
-        });
-    }
+//     public Intersects()
+//     {
+//         Timer t = new Timer(1000, new ActionListener() {
+//             public void actionPerformed(ActionEvent e) {
+//                 if(g2D.hit(hitBox, rectangle, false))
+//                     System.out.println("They are intersecting!");
+//             }
+//         });
+//     }
 }
--- Snake.java
+++ Snake.java
@@ -8,19 +8,22 @@ public class Snake
     public final static boolean inDebugMode = true;
     final static int X = 1126;
     final static int Y = 680;
-    public static JPanel gamePanel = new JPanel();
     public static JFrame gameWindow = new JFrame("Snake");
     public static Game rect1 = new Game(0,Y/2,Color.BLACK,75,17);
     public static Game rect2 = new Game(X/2,Y/2,Color.BLACK,50,10);
     public static void main(String[] args)
     {
+        Container gameContentPane = gameWindow.getContentPane();
+        // NEED TO USE FLOW LAYOUT BUT DOESNT WORK AS IS gameContentPane.setLayout(new FlowLayout());
         gameWindow.setSize(X, Y);
         gameWindow.setLocationRelativeTo(null);
-        gameWindow.setFocusable(true);
-        gameWindow.setFocusTraversalKeysEnabled(false);
+        gameContentPane.setFocusable(true);
+        gameContentPane.setFocusTraversalKeysEnabled(false);
         new keyHandler();
-        gameWindow.add(rect1);
-        gameWindow.add(rect2);
+        gameContentPane.add(rect1);
+        gameContentPane.add(rect2);
         gameWindow.setVisible(true);
+        gameContentPane.setVisible(true);
+        //new Intersects();
     }
 }
